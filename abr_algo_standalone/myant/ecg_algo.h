5d08e4dc9ab2bfb6f2efaf13cf7910b9a406d66b
glor-w <gloria.wu@myant.ca>
2024-05-29 11:10:19 -0400

    change ABR_INPUT_BASELINE_VALUE to float

--- abr_algo_standalone/myant/ecg_algo.h
+++ abr_algo_standalone/myant/ecg_algo.h
@@ -3,23 +3,24 @@
 #define ABR_RPEAK_MODEL_HPP_
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-#include <stdint.h>
+#include "abr_preprocess.h"
+#include "data_processing.h"
 #include <stdbool.h>
 #include <stddef.h>
-#include "data_processing.h"
-#include "abr_preprocess.h"
+#include <stdint.h>
 
-#define ECG_ALGO_OUTPUT_SIZE    1
-//ABR model definitions
-#define ABR_INPUT_BASELINE_VALUE        685.7142857142857l
+#define ECG_ALGO_OUTPUT_SIZE     1
+// ABR model definitions
+#define ABR_INPUT_BASELINE_VALUE 685.7142857f
 
-void ecg_algo_update_garmentid(garment_id_e id);
-void ecg_algo_init(void);
-bool ecg_algo_run(float *data, uint8_t ch_count, bool restart);
-void ecg_algo_get_output(float *outputs, uint8_t len);
+    void ecg_algo_update_garmentid(garment_id_e id);
+    void ecg_algo_init(void);
+    bool ecg_algo_run(float *data, uint8_t ch_count, bool restart);
+    void ecg_algo_get_output(float *outputs, uint8_t len);
 
 #ifdef __cplusplus
 }
