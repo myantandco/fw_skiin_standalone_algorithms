63eebaea24c45a2f92fc1ca1c6fde68cdcdb84d9
glor-w <gloria.wu@myant.ca>
2024-06-20 14:59:24 -0400

    add dummy inputs to read only one channel of csv
    change ecg_reduced data to int16_t from uint16_t

--- ecg_bit_reduction/main.c
+++ ecg_bit_reduction/main.c
@@ -1,29 +1,27 @@
-#include "ecg_bit_reduction.h"
 #include "csv_writers.h"
+#include "ecg_bit_reduction.h"
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
-#define ECG_DATA_BUFFER_SIZE         24
+#define ECG_DATA_BUFFER_SIZE 24
 
 int main(int argc, const char *argv[])
 {
     // Inputs
     float dpInput[MAX_ROWS] = {0};
+    float dpDummyInput1[2]  = {0};
+    float dpDummyInput2[2]  = {0};
     int   bNumRows          = 0;
 
     // Intermediate data
-    bool fECGBufferRefresh = true;
-    uint16_t bECGReducedData = 0;
-
-    // Write CSV header - TODO: Gloria
-    const char *pVarNames = "output";
-    CSVW_WriteCSVHeader("out.csv", pVarNames);
+    bool    fECGBufferRefresh = true;
+    int16_t bECGReducedData   = 0;
 
     // Read Inputs from CSV:
     printf("Setting inputs...\r\n");
-    CSVW_ReadCSV(INPUT_FILE_NAME, dpInput, NULL, NULL, &bNumRows);
+    CSVW_ReadCSV(INPUT_FILE_NAME, dpInput, dpDummyInput1, dpDummyInput2, &bNumRows);
 
     // Loop through entire input array
     printf("Looping through input data...\r\n\r\n");
@@ -32,8 +30,9 @@ int main(int argc, const char *argv[])
         // ECG Bit Reduction Algorithm
         bECGReducedData = ECGBitReduction_SampleReduction((uint32_t)dpInput[i], ECG1, fECGBufferRefresh);
 
-        // Write algorithm output to CSV 
-        CSVW_WriteCSVSingle("out.csv", bECGReducedData, ECG1);
+        // Write algorithm output to CSV
+        CSVW_WriteCSVSingle("in_ch1.csv", dpInput[i], ECG3);
+        CSVW_WriteCSVSingle("out1.csv", bECGReducedData, ECG3);
 
         // Only refresh on the first iteration of the loop
         fECGBufferRefresh = false;
